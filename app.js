883208e4f88eec7a848953a224931d5148aec59b
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-10-04 09:47:23 -0400

    done

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const fs = require('fs')
 const fetch = require('node-fetch')
 
 
-const randomOffset = () => Math.random()*18000;
-
+const randomOffset = () => Math.floor(Math.random()*18000)*1;
 (async function () {
-    const response = await fetch(api)
+    const response = await fetch(api+randomOffset())
+    console.log(randomOffset())
+  
     const data = await response.json()
     const categories = JSON.stringify(data)
-    console.log(data)
+   
     fs.writeFile('categories.json',categories, (err)=>{
         if(err) throw err;
         console.log('the file has been saved!');
